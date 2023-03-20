768cc3ce0d33e4e1bdaa833d60b6082f53b2aa47
Shail Murtaza <shailmurtaza@gmail.com>
2023-03-21 00:26:05 +0500

    Replaced orignal course urls with my own URLs

--- flask_app/web.py
+++ flask_app/web.py
@@ -1,18 +1,71 @@
 #!/usr/bin/python3
 
 from flask import Flask, redirect, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+app.app_context().push()
+
+class Users(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, unique=True, nullable=False)
+    email = db.Column(db.Text, unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+    firstname = db.Column(db.Text, nullable=False)
+    lastname = db.Column(db.Text, nullable=True)
+
+    country = db.Column(db.Text)
+    company = db.Column(db.Text)
+    street = db.Column(db.Text)
+    addr2 = db.Column(db.Text)
+    town = db.Column(db.Text)
+    postcode = db.Column(db.Integer)
+
+    phone = db.Column(db.Text)
+
+    paid = db.Column(db.Boolean, default=True)
+    image_file = db.Column(db.Text, nullable=False, default='user.jpg')
+
+    def __str__(self):
+        return f'{self.email} : {self.username}'
+
+    def __repr__(self):
+        return f'{self.email} : {self.username}'
+
+
+class Units(db.Model):
+    __tablename__ = 'Units'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    template = db.Column(db.Text)
+
+
+class Unit_Sections(db.Model):
+    __tablename__ = 'Unit_sections'
+    id = db.Column(db.Integer, primary_key=True)
+    unit_id = db.Column(db.Integer)
+    title = db.Column(db.Text)
+    template = db.Column(db.Text)
+
+
 @app.route('/')
 def index():
-	return redirect("/lms/care-certificate")
+    return redirect("/lms/care-certificate")
 
 
 @app.route("/lms/care-certificate")
 def care_certificate():
-	return render_template("care_certificate/care_certificate.html")
+    return render_template("care_certificate/care_certificate.html")
 
 
-app.run(port=8081, debug=True)
+@app.route("/lms/care-certificate/unit/<int:unit_id>")
+def unit(unit_id):
+    return str(unit_id)
+
+if __name__ == "__main__":
+    app.run(port=8081, debug=True)
 
