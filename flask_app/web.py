4678e4426ef4e4a6240cd28e79701c59bc75c482
ShailMahan012 <shailmahan012@gmail.com>
2023-03-20 04:52:11 -0400

    Added index file of care course

--- flask_app/web.py
+++ flask_app/web.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, redirect, render_template
+
 
 app = Flask(__name__)
-
-@app.route("/")
+@app.route('/')
 def index():
-	return "Shail"
+	return redirect("/lms/care-certificate")
+
+
+@app.route("/lms/care-certificate")
+def care_certificate():
+	return render_template("care_certificate/care_certificate.html")
+
 
 app.run(port=8081, debug=True)
 
