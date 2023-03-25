162a4b4d1fd9c2c249918e29eb78ecbd6d688e22
ShailMahan012 <shailmahan012@gmail.com>
2023-03-25 10:11:20 +0500

    Sidebar links correction

---
+++ change_topic_links_sidebar.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup as bs 
+
+def get_soup(html):
+    soup = bs(html, "html.parser")
+    return soup
+
+course_layout = "flask_app/templates/care_certificate/course_layout.html"
+
+soup = get_soup(open(course_layout).read())
+
+all_units = soup.find_all("ul")
+
+
+for ic, unit in enumerate(all_units):
+    unit_no = ic + 1
+    topics = unit.find_all("a", {"class": "topic-completed"})
+    
+    for ix, topic_link in enumerate(topics):
+        topic_no = ix + 1
+        topic_link["href"] = f"/lms/care-certificate/unit/{unit_no}/topic/{topic_no}"
+
+open(course_layout, "w").write(str(soup))
+
+# check http://127.0.0.1:8081/lms/care-certificate/unit/8/topic/50
