fa0c0d9b8b7915b781e35a46d6c38372eb848e9a
Shail Murtaza <shailmurtaza@gmail.com>
2023-03-26 15:34:02 +0500

    Quiz Scraping Done. Quiz checking remaining

--- scrap_quiz.py
+++ scrap_quiz.py
@@ -14,15 +14,66 @@ pickle_in.close()
 
 
 def get_soup(url):
-    resp = session.get(url)
-    soup = bs(resp.content, "html.parser")
-    open("test.html", "wb").write(resp.content)
-    return soup, resp.content
+    # resp = session.get(url)
+    # content = resp.content
+    content = open("test.html", "rb").read()
+    soup = bs(content, "html.parser")
+    # open("test.html", "wb").write(content)
+    return soup, content
+
+
+def write_HTML(HTML, unit_no):
+    # file = open(f"units/unit_{unit_no}/quiz.html", "w")
+    file = open(f"flask_app/templates/care_certificate/quiz_example.html", "w")
+    file.write(HTML)
+    file.close()
+
+
+def get_quiz(soup):
+    quiz = soup.find("ol", {"class": "wpProQuiz_list"})
+    for li in quiz.find_all("li", recursive=False):
+        del li["style"]
+        li.find("div", {"class": "wpProQuiz_question_page"}).extract()
+        li.find("h5", {"class": "wpProQuiz_header"}).extract()
+    # exit()
+    return quiz.prettify()
+
+
+layout = """
+<link rel="stylesheet" href="/static/quiz.css">
+{{% extends 'care_certificate/quiz_layout.html' %}}
+
+{{% block top_box %}}
+{}
+{{% endblock %}}
+
+{{% block audio %}}
+{}
+{{% endblock %}}
+
+{{% block quiz %}}
+{}
+{{% endblock %}}
+"""
 
 for unit_no in range(1, 16):
     unit_dir = f"units/unit_{unit_no}"
     url = open(f"{unit_dir}/quiz_links.txt").readline()[:-1]
 
-    soup = get_soup(url)
+    soup, content = get_soup(url)
+
+    top_box = soup.find("div", {"class": "wpb_column vc_column_container vc_col-sm-12"}) # this
+
+    # print(top_box)
+    audio = soup.find("div", {"id": "skin_default"}) # this
+
+    # print(audio)
+    quiz = get_quiz(soup) # this
+    # print(quiz)
+
+    HTML = layout.format(str(top_box), str(audio), str(quiz))
+    write_HTML(HTML, unit_no)
+
     print(f"DONE: {unit_no}")
     break
+
