3cc449e678909d772966652f1beadf37cf46f5e5
ShailMahan012 <shailmahan012@gmail.com>
2023-03-28 10:00:47 +0500

    add_to_DB.py. Updated models in DATABASE Also created database

---
+++ add_to_DB.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append("flask_app")
+
+from models import *
+
+# db.drop_all()
+# db.create_all()
+# exit()
+
+
+unit_titles_file = open("units/unit_titles.txt")
+unit_titles = unit_titles_file.read().split("\n")[:-1]
+unit_titles_file.close()
+
+for title in unit_titles:
+    unit = Unit(title=title)
+    db.session.add(unit)
+
+
+for unit_no in range(1, 16):
+    unit_dir = f"units/unit_{unit_no}"
+    topic_file = open(f"{unit_dir}/topic_titles.txt")
+    quiz_file = open(f"{unit_dir}/quiz_titles.txt")
+
+
+    topic_titles = topic_file.read().split("\n")[:-1]
+    quiz_title = quiz_file.read().split("\n")[:-1][0]
+
+    topic_file.close()
+    quiz_file.close()
+
+    for title in topic_titles:
+        section = Section(unit_id=unit_no, title=title, type="topic")
+        db.session.add(section)
+
+    # Only one quiz
+    section = Section(unit_id=unit_no, title=quiz_title, type="quiz")
+    db.session.add(section)
+
+db.session.commit()
---
+++ flask_app/models.py
@@ -0,0 +1,57 @@
+from flask import Flask, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+
+class Users(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, unique=True, nullable=False)
+    email = db.Column(db.Text, unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+    firstname = db.Column(db.Text, nullable=False)
+    lastname = db.Column(db.Text, nullable=True)
+
+    country = db.Column(db.Text)
+    company = db.Column(db.Text)
+    street = db.Column(db.Text)
+    addr2 = db.Column(db.Text)
+    town = db.Column(db.Text)
+    postcode = db.Column(db.Integer)
+
+    phone = db.Column(db.Text)
+
+    paid = db.Column(db.Boolean, default=True)
+    image_file = db.Column(db.Text, nullable=False, default='user.jpg')
+
+    def __str__(self):
+        return f'{self.email} : {self.username}'
+
+    def __repr__(self):
+        return f'{self.email} : {self.username}'
+
+
+class Unit(db.Model):
+    __tablename__ = 'Unit'
+    unit_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+
+class Section(db.Model):
+    __tablename__ = 'Section'
+    section_id = db.Column(db.Integer, primary_key=True)
+    unit_id = db.Column(db.Integer)
+    title = db.Column(db.Text)
+    type = db.Column(db.Text)
+
+class Progress(db.Model):
+    __tablename__ = 'Progress'
+    progress_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    section_id = db.Column(db.Integer)
+    status = db.Column(db.Boolean, default=False)
